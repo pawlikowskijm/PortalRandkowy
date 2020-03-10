3c15bf1d73e91b08dab21540391f96e6f09af7fb
pawlikowskijm <pawlikowskijm@gmail.com>
2020-03-10 13:48:07 +0100

    First angular module creation

---
+++ PortalRandkowy.SPA/src/app/value/value.module.ts
@@ -0,0 +1,20 @@
+// Angular Imports
+import { NgModule } from '@angular/core';
+
+// This Module's Components
+import { ValueComponent } from './value.component';
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        ValueComponent,
+    ],
+    exports: [
+        ValueComponent,
+    ]
+})
+export class ValueModule {
+
+}
